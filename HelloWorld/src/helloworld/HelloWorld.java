3fa635a5b0a53952ece51d49908a02d9456d9c15
mbhenry0 <mbhenry0@CC270-86291.frostburg.edu>
2016-10-12 09:50:00 -0400

    first commit

--- HelloWorld/src/helloworld/HelloWorld.java
+++ HelloWorld/src/helloworld/HelloWorld.java
@@ -5,6 +5,13 @@
  */
 package helloworld;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 /**
  *
  * @author mbhenry0
@@ -14,8 +21,26 @@ public class HelloWorld {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args){
+        try{
+        ArrayList<String>lines=new ArrayList<String>();
         System.out.println("hello World");
+        FileReader F= new FileReader(args[0]);
+        BufferedReader Read= new BufferedReader(F);
+        String Line="";
+        while((Line=Read.readLine())!=null){
+            lines.add(Line);
+        }
+        for(String S:lines){
+            System.out.println("__--->%S%n"+ S);
+        }
     }
+        catch(FileNotFoundException FNF){
+            System.err.println("File Not Found");
+        }
+        catch(IOException io){
+            System.err.println("Error");
+        }
     
+}
 }
